ba389eb7da33b62a47cb6722b24744386bde7340
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-10 01:57:53 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,8 +1,15 @@
 class Waiter
   attr_reader :name, :years
 
+  @@all = []
+
   def initialize(name, years)
     @name = name
     @years = years
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
